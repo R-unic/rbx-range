c98d0048ad04b14c9917e2d9d610c1778c720656
R-unic <roleyspersonal@gmail.com>
2024-12-02 11:35:00 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Range
+A simple range implementation with utility methods
+
+## Some of the methods
+`clamp()`
+`randomNumber()`
+`randomInteger()`
+`isNumberWithin()`
+`isNumberExclusivelyWithin()`
+`isRangeWithin()`
+`isRangeExclusivelyWithin()`
+...and more
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+const { clamp, min, max } = math;
+
+type Maybe<T> = T | undefined;
+export interface RangeJSON {
+	minimum: number;
+	maximum: number;
+}
+
+export class Range {
+	public constructor(
+		public readonly minimum: number,
+		public readonly maximum: number
+	) { }
+
+	public static fromJSON(data: RangeJSON): Range {
+		return new Range(data.minimum, data.maximum);
+	}
+
+	public clamp(n: number): number {
+		return clamp(n, this.minimum, this.maximum);
+	}
+
+	public randomInteger(): number {
+		return (new Random).NextInteger(this.minimum, this.maximum);
+	}
+
+	public randomNumber(): number {
+		return (new Random).NextNumber(this.minimum, this.maximum);
+	}
+
+	public isNumberWithin(n: number): boolean {
+		return n >= this.minimum && n <= this.maximum;
+	}
+
+	public isNumberExclusivelyWithin(n: number): boolean {
+		return n > this.minimum && n < this.maximum;
+	}
+
+	public isRangeWithin(other: Range): boolean {
+		return this.maximum >= other.minimum && this.minimum <= other.maximum;
+	}
+
+	public isRangeExclusivelyWithin(other: Range): boolean {
+		return this.minimum <= other.minimum && this.maximum >= other.maximum;
+	}
+
+	public union(other: Range): Range {
+		return new Range(
+			min(this.minimum, other.minimum),
+			max(this.maximum, other.maximum)
+		);
+	}
+
+	public intersection(other: Range): Maybe<Range> {
+		const minimum = max(this.minimum, other.minimum);
+		const maximum = min(this.maximum, other.maximum);
+		return minimum <= maximum ? new Range(minimum, maximum) : undefined;
+	}
+
+	public center(): number {
+		return (this.minimum + this.maximum) / 2;
+	}
+
+	public size(): number {
+		return this.maximum - this.minimum;
+	}
+
+	public toJSON(): RangeJSON {
+		return { minimum: this.minimum, maximum: this.maximum };
+	}
+
+	public toString(): string {
+		return `${this.minimum}..${this.maximum}`;
+	}
+}
